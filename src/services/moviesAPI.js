de8336a703e10807457d3e1f56dd8729f1789218
Veronika Sirak <7nicka@gmail.com>
2022-07-03 01:12:54 +0100

    fix moviesAPI

--- src/services/moviesAPI.js
+++ src/services/moviesAPI.js
@@ -6,34 +6,34 @@ const KEY = '0b8103dd54def1b2724a61ab95182ae5';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 export const fetchTrendMovies = async () => {
-  const response = await axios.get(`/trending/movie/day?api_key=${KEY}`);
-  return response.data;
+  const { data } = await axios.get(`/trending/movie/day?api_key=${KEY}`);
+  return data;
 };
 
 export const fetchMovieById = async movieId => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${KEY}&language=en-US`
+  const { data } = await axios.get(
+    `/movie/${movieId}?api_key=${KEY}&language=en-US`
   );
-  return response.data;
+  return data;
 };
 
 export const fetchSearchMovieByQuery = async queryName => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=en-US&query=${queryName}&page=1&include_adult=false`
+  const { data } = await axios.get(
+    `/search/movie?api_key=${KEY}&language=en-US&query=${queryName}&page=1&include_adult=false`
   );
-  return response.data;
+  return data;
 };
 
 export const fetchMovieCast = async movieId => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${KEY}&language=en-US`
+  const { data } = await axios.get(
+    `/movie/${movieId}/credits?api_key=${KEY}&language=en-US`
   );
-  return response.data;
+  return data;
 };
 
 export const fetchMovieReviews = async movieId => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${KEY}&language=en-US&page=1`
+  const { data } = await axios.get(
+    `/movie/${movieId}/reviews?api_key=${KEY}&language=en-US&page=1`
   );
-  return response.data;
+  return data;
 };
